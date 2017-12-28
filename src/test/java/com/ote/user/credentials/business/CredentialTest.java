a46746779faa691ac15ec3ef547363f843cc945b
OTerrien <oterrien@neuf.fr>
2017-12-28 13:58:40 +0100

    update test

--- src/test/java/com/ote/user/credentials/business/CredentialTest.java
+++ src/test/java/com/ote/user/credentials/business/CredentialTest.java
@@ -21,17 +21,7 @@ import java.util.stream.StreamSupport;
 
 public class CredentialTest {
 
-    @ParameterizedTest
-    @CsvSource({
-            "test, ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff",
-            "toto, 10e06b990d44de0091a2113fd95c92fc905166af147aa7632639c41aa7f26b1620c47443813c605b924c05591c161ecc35944fc69c4433a49d10fc6b04a33611",
-            "monPassword12DD;DEF, 8250780b2e29b8d08b6384025a91b082e328d6ee57f77c42033ec9180fef17bc34939b7c4bd813dae72eb6423f4f586582455c23f410796abaa4c1f3905dc0d8"})
-    public void testEncryption(String password, String expectation) {
-        Assertions.assertThat(Encryptor.getInstance().encrypt(password)).isEqualTo(expectation);
-    }
-
-    @Test
-    @Disabled
+    //@Test
     public void generateNewFile() throws IOException {
 
         Path path = Paths.get("./src/test/resources", "com/ote/user/credentials/business", "sampleEncryptedPasswords.csv");
@@ -49,11 +39,10 @@ public class CredentialTest {
                 }
             }
         }
-
     }
 
     @Test
-    public void testThread() {
+    public void testEncryption() {
 
         Scanner scanner = new Scanner(ClassLoader.getSystemResourceAsStream("com/ote/user/credentials/business/sampleEncryptedPasswords.csv"));
         streamScanner(scanner).
